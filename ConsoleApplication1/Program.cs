dd7db43d59d3413b1b8c5e9f909a9629541b89d6
williamhhk <william.han@gmail.com>
2016-07-12 18:09:36 -0700

    Test
    Test

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime adDate = new DateTime(1601, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            DateTime utcDate = DateTime.FromFileTimeUtc(0);
+            bool equal = adDate == utcDate;
+        }
+    }
+}
